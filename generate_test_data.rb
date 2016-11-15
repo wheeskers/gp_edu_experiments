8ea0b7b87101a4d9956a16878d555910238c7140
Wheeskers <wheeskers@users.noreply.github.com>
2016-11-15 19:09:26 +0300

    Added script for test data files generation

---
+++ generate_test_data.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+require 'time'
+
+data_files = [ 'some_data0.csv',
+               'some_data1.csv',
+               'some_data2.csv',
+               'some_data3.csv',
+               'some_data4.csv'  ]
+
+time_stamp       = Time::new
+time_stamp_start = Time::parse("2016-10-20 08:20:00")
+time_stamp_stop  = Time::parse("2016-10-20 13:40:00")
+
+data_files.each do |out_file|
+  time_stamp = time_stamp_start
+  some_value = rand(-100..100)
+  File::open("./#{out_file}",'w+') do |f|
+    while time_stamp <= time_stamp_stop
+      f.write "#{time_stamp.strftime('%Y-%m-%d %H:%M:%S')};#{some_value}\n"
+      some_value += rand(50)
+      some_value -= rand(50)
+      time_stamp += 60
+    end
+  end
+end
